ebe305c31c0b40972fa1eda23f6294a3cd037f33
Jonathon Powell <mail@jonathonpowell.net>
2020-06-29 21:50:53 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,5 +1,5 @@
 module.exports = async function(msg){
     msg.reply({embed: {
-        description: "Check out my source code: <link>\nFollow my original creator on YouTube: https://www.youtube.com/channel/UC4fs_zKuE1lsXbCLS_Y9UTA",
+        description: "Check out my source code: https://github.com/2JJ1/Private-Discord-Bot\nFollow my original creator on YouTube: https://www.youtube.com/channel/UC4fs_zKuE1lsXbCLS_Y9UTA",
     }})
 }
