987e1b0e3732bb98e894fdad1b3a7333c9d6ba2c
2JJ1 <jonathonpo21@gmail.com>
2021-08-18 15:21:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -203,8 +203,9 @@ client.on('message', async msg => {
 
 						//Send response
 						if(typeof autoResponse === "object"){
-							if(autoResponse.actions.indexOf("delete") !== -1) msg.delete({reason: "Instructed by the pattern matcher"})
-							if(autoResponse.actions.indexOf("mute") !== -1) MuteMember({msg, reason: "Instructed by the pattern matcher", by: "bot"}).catch(()=>{})
+							if(autoResponse.actions.indexOf("delete") !== -1) msg.delete({reason: "Instructed by the message pattern matcher"})
+							if(autoResponse.actions.indexOf("kick") !== -1) msg.member.kick({reason: "Instructed by the message pattern matcher"})
+							if(autoResponse.actions.indexOf("mute") !== -1) MuteMember({msg, reason: "Instructed by the message pattern matcher", by: "bot"}).catch(()=>{})
 							if(autoResponse.response) responders.dmPreferreds[i] ? CleanRespond(msg, autoResponse.response) : msg.reply(autoResponse.response)
 						}
 						else responders.dmPreferreds[i] ? CleanRespond(msg, autoResponse) : msg.reply(autoResponse)
