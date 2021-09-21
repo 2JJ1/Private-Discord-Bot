769598bcfe0ad3fe8f106fd229580df18de33855
2JJ1 <jonathonpo21@gmail.com>
2021-09-21 12:07:41 -0500

    Discord.js v13 upgrade

--- my_modules/DiscordJsBotClient.js
+++ my_modules/DiscordJsBotClient.js
@@ -1,5 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client, Intents} = require('discord.js');
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+    ]
+});
 
 //Created a whole module so I could reuse the object
 module.exports = client
