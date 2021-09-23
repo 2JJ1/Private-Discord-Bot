6e73c7c6b4c876aa8f408ebc96c22dbeff92309b
2JJ1 <jonathonpo21@gmail.com>
2021-09-23 09:50:12 -0500

    Fixed listmods

--- commands/listmods.js
+++ commands/listmods.js
@@ -4,10 +4,10 @@ module.exports = {
 	data: new SlashCommandBuilder()
         .setName("listmods")
         .setDescription('List all of the mini-moderators, moderators, and admins in this guild.'),
-	async execute(msg){
+	async execute(interaction){
 		var list = "Admins: ";
 		//Grabs list of members with the ADMINISTRATOR permission or admin role (Except for bots)
-		var admins = (await msg.guild.members.fetch()).filter(member => { 
+		var admins = (await interaction.guild.members.fetch()).filter(member => { 
 			return (
 				(member.permissions.has("ADMINISTRATOR") || member.roles.cache.find(roles => roles.name.toLowerCase() === 'admin')) && 
 				member.user.bot === false
@@ -17,18 +17,18 @@ module.exports = {
 		list += admins.join(", ")
 
 		//Lists people with the moderator role (if any)
-		let moderators = (await msg.guild.members.fetch()).filter(member => member.roles.cache.find(roles => roles.name.toLowerCase() === 'moderator') && member.user.bot === false)
+		let moderators = (await interaction.guild.members.fetch()).filter(member => member.roles.cache.find(roles => roles.name.toLowerCase() === 'moderator') && member.user.bot === false)
 		.map(member =>  `<@${member.user.id}>`) //Converts to mentions
 		if(moderators.length > 0) list += "\r\nModerators: " + moderators.join(", ")
 
 		//List people with the mini-moderator role (If any)
-		let miniModerators = (await msg.guild.members.fetch()).filter(member => member.roles.cache.find(roles => roles.name.toLowerCase() === 'mini-moderator') && member.user.bot === false)
+		let miniModerators = (await interaction.guild.members.fetch()).filter(member => member.roles.cache.find(roles => roles.name.toLowerCase() === 'mini-moderator') && member.user.bot === false)
 		.map(member =>  `<@${member.user.id}>`) //Converts to mentions
 		if(miniModerators.length > 0) list += "\r\nMini-Moderators: " + miniModerators.join(", ")
 						
-		msg.channel.send({embed: {
+		interaction.reply({embeds: [{
 			color: 3447003,
 			description: list
-		}});
+		}]})
 	}
 }
