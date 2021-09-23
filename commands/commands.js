9360cb1c633d9a84438a02125ed0175f6c023044
2JJ1 <jonathonpo21@gmail.com>
2021-09-23 09:51:31 -0500

    Fixed commands list

--- commands/commands.js
+++ commands/commands.js
@@ -4,16 +4,16 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("commands")
         .setDescription('Responds with the list of commands.'),
-    execute(msg){
-        msg.channel.send({
-            embed: {
+    execute(interaction){
+        interaction.reply({
+            embeds: [{
                 fields: [
                     {
                         name: "Public Commands List",
                         value: "!8ball <question>, !joke, !meme, !roast, !compile <coding_language> <code>, !whois <@mention>, !guildinfo, !listmods, !ping, !about"
                     }
                 ],
-            }
+            }]
         })
     },
 }
