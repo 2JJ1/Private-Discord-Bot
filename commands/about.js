1305631f74b2320ad2088ef4c5fcc97867f9e47d
2JJ1 <jonathonpo21@gmail.com>
2021-09-23 09:52:16 -0500

    Fixed about command

--- commands/about.js
+++ commands/about.js
@@ -4,9 +4,9 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("about")
         .setDescription('Lets talk about me.'),
-    async execute(msg){
-        msg.reply({embed: {
+    async execute(interaction){
+        interaction.reply({embeds: [{
             description: "Check out my source code: https://github.com/2JJ1/Private-Discord-Bot\nFollow my original creator on YouTube: https://www.youtube.com/channel/UC4fs_zKuE1lsXbCLS_Y9UTA",
-        }})
+        }]})
     }
 }
