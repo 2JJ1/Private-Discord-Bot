bd39fa604f3ed556e3b061af125f25600a3c5afe
2JJ1 <jonathonpo21@gmail.com>
2021-10-04 11:53:42 -0500

    Update cleanrespond.js

--- my_modules/cleanrespond.js
+++ my_modules/cleanrespond.js
@@ -1,5 +1,5 @@
 //Try to respond by DM, otherwise send to text channel
 module.exports = function CleanRespond(msgObj, text){
 	msgObj.author.send(text)
-	.catch(()=> msg.channel.send(`<@msg.author.id> ${text}`))
+	.catch(()=> msgObj.channel.send(`<@msg.author.id> ${text}`))
 }
